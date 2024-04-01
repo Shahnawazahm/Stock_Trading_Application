4c65f8a23a3dde35c8cfe4223365dbed61fdca97
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2024-04-01 18:09:39 +0530

    Delete StockTrading directory

--- StockTrading/src/main/java/com/shahprojects/stocktrading/repository/OrderMasterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shahprojects.stocktrading.repository;
-
-import com.shahprojects.stocktrading.entity.OrderMaster;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderMasterRepository extends JpaRepository<OrderMaster, Long>{
-}
--- StockTrading/src/main/java/com/shahprojects/stocktrading/repository/TradeDetailsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shahprojects.stocktrading.repository;
-
-import com.shahprojects.stocktrading.entity.TradeDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TradeDetailsRepository extends JpaRepository<TradeDetails,Long>{
-}
